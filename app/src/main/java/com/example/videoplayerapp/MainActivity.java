e245764386a52c5ea7e272617eec20f0272b76b4
Angel Travieso <angeltraviesoc@gmail.com>
2022-12-01 09:45:57 -0400

    reproducir videos de internet (fetching)

--- app/src/main/java/com/example/videoplayerapp/MainActivity.java
+++ app/src/main/java/com/example/videoplayerapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.videoplayerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.MediaController;
 import android.widget.VideoView;
@@ -9,7 +10,7 @@ import android.widget.VideoView;
 public class MainActivity extends AppCompatActivity {
 
     // declarando widgets
-    VideoView videoView;
+    VideoView videoView, videoView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,9 +19,9 @@ public class MainActivity extends AppCompatActivity {
 
         // inicializando widgets
         videoView = findViewById(R.id.video_view);
+        videoView2 = findViewById(R.id.video_view2);
 
         // obteniendo video del almacenamiento local (dispositivo)
-
 
         // Este metodo especifica la carpeta y el nombre del video
         videoView.setVideoPath(
@@ -31,6 +32,18 @@ public class MainActivity extends AppCompatActivity {
         MediaController mc = new MediaController(this);
         mc.setAnchorView(videoView);
         videoView.setMediaController(mc);
+
+
+        // Fetch data y mostrar videos de internet (links)
+        Uri uri = Uri.parse("https://static.videezy.com/system/resources/previews/000/002/231/original/5226496.mp4");
+        videoView2.setVideoURI(uri);
+
+        MediaController mc2 = new MediaController(this);
+        mc2.setAnchorView(videoView2);
+        videoView2.setMediaController(mc2);
+
+        // reproducir el video cuando se lanza la app
+        videoView2.start();
 
     }
 }
